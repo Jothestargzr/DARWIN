982f3b677bde4d6cc546403a5974011ee156527f
Jothestargzr <kwameduah789@gmail.com>
2026-08-11 03:51:34 +0100

    Fix macOS 15 / Xcode 16 sys/fileport.h header compatibility in mojo C++ patchkit

---
+++ BrowserOS/packages/browseros/chromium_patches/mojo/core/ipcz_driver/wrapped_platform_handle.cc
@@ -0,0 +1,19 @@
+diff --git a/mojo/core/ipcz_driver/wrapped_platform_handle.cc b/mojo/core/ipcz_driver/wrapped_platform_handle.cc
+index 0000000000000..1111111111111 100644
+--- a/mojo/core/ipcz_driver/wrapped_platform_handle.cc
++++ b/mojo/core/ipcz_driver/wrapped_platform_handle.cc
+@@ -24,7 +24,14 @@
+ 
+ #if BUILDFLAG(IS_MAC)
+ #include <mach/mach.h>
++#if __has_include(<sys/fileport.h>)
+ #include <sys/fileport.h>
++#else
++extern "C" {
++int fileport_makeport(int fd, mach_port_t *port);
++int fileport_makefd(mach_port_t port);
++}
++#endif
+ #endif
+ 
+ namespace mojo::core::ipcz_driver {
